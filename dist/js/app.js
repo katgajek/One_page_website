5e14fff08b333ae771b1918ec39fb67e2e6773ff
Katarzyna Gajek <katgajek@gmail.com>
2018-02-01 23:12:00 +0100

    change cert secttion, improve RWD

--- dist/js/app.js
+++ dist/js/app.js
@@ -84,6 +84,14 @@ $(function(){
         ]
     });
 
-    //
+    //cert slideDown
+
+    const $certBtn = $(".certBtn");
+
+    $certBtn.on("click tap", function(){
+           $(this).parent().next().slideToggle(500);
+
+
+    })
 
 });
